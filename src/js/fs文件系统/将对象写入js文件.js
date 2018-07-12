cf22b8816d695c38aa87b7ba558bc210424fdd3d
cag2050 <cag2050@163.com>
2018-07-12 09:45:19 +0800

    m

--- src/js/fs文件系统/将对象写入js文件.js
+++ src/js/fs文件系统/将对象写入js文件.js
@@ -1,14 +1,19 @@
 var fs = require("fs");
 
+let obj = {
+    name: 'cag2050',
+    age: 30
+}
+
 console.log("准备写入文件");
-fs.writeFile('写入文件.txt', '我是通过 fs.writeFile 写入文件的内容', function (err) {
+fs.writeFile('obj_str.js', obj.toString(), function (err) {
     if (err) {
         return console.error(err);
     }
     console.log("数据写入成功！");
     console.log("--------我是分割线-------------")
     console.log("读取写入的数据！");
-    fs.readFile('写入文件.txt', function (err, data) {
+    fs.readFile('obj_str.js', function (err, data) {
         if (err) {
             return console.error(err);
         }
